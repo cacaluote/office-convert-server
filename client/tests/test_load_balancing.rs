128544af52f546f38f97fdc79cab35e0bc265a81
Jacobtread <jacobtread@gmail.com>
2025-05-29 00:49:55 +1200

    feat: load balancing test using testcontainers

--- client/tests/test_load_balancing.rs
+++ client/tests/test_load_balancing.rs
@@ -1,43 +1,61 @@
-// use bytes::Bytes;
-// use office_convert_client::{OfficeConvertClient, OfficeConvertLoadBalancer};
-// use std::sync::Arc;
-// use tokio::sync::Barrier;
+use bytes::Bytes;
+use office_convert_client::{OfficeConvertClient, OfficeConvertLoadBalancer};
+use std::sync::Arc;
+use testcontainers::{
+    core::{wait::HttpWaitStrategy, IntoContainerPort, WaitFor},
+    runners::AsyncRunner,
+    GenericImage, ImageExt,
+};
+use tokio::sync::Barrier;
 
-// #[tokio::test(flavor = "multi_thread")]
-// #[ignore = "requires first starting a office converter server"]
-// async fn attempt_spam() {
-//     // Number of load balancers
-//     let sets = 1;
+#[tokio::test(flavor = "multi_thread")]
+async fn attempt_spam() {
+    let container = GenericImage::new("jacobtread/office-convert-server", "latest")
+        .with_exposed_port(3000.tcp())
+        .with_wait_for(WaitFor::http(
+            HttpWaitStrategy::new("/status").with_expected_status_code(200u16),
+        ))
+        .with_env_var("RUST_LOG", "debug")
+        .start()
+        .await
+        .unwrap();
 
-//     // Number of "Convert this" per "set"
-//     let tasks = 10;
+    let host = container.get_host().await.unwrap();
+    let host_port = container.get_host_port_ipv4(3000).await.unwrap();
+    let client_url = format!("http://{host}:{host_port}");
 
-//     // Simple thing for coordinating tasks "Keep the test running until this many things calls .wait()"
-//     let barrier = Arc::new(Barrier::new((tasks * sets) + 1));
+    // Number of load balancers
+    let sets = 1;
 
-//     // Load the file to process
-//     let file = Bytes::from_static(include_bytes!("samples/sample.docx"));
+    // Number of "Convert this" per "set"
+    let tasks = 10;
 
-//     for set in 0..sets {
-//         // Setup a client to put into the load balancer
-//         let client = OfficeConvertClient::new("http://localhost:3000").unwrap();
+    // Simple thing for coordinating tasks "Keep the test running until this many things calls .wait()"
+    let barrier = Arc::new(Barrier::new((tasks * sets) + 1));
 
-//         // Setup the load balancer
-//         let lb = OfficeConvertLoadBalancer::new([client]);
-//         let lb = Arc::new(lb);
+    // Load the file to process
+    let file = Bytes::from_static(include_bytes!("samples/sample.docx"));
 
-//         for task in 0..tasks {
-//             let lb = lb.clone();
-//             let barrier = barrier.clone();
-//             let file = file.clone();
-//             tokio::spawn(async move {
-//                 println!("start job set = {set}, task = {task}");
-//                 lb.convert(file).await.unwrap();
-//                 println!("end job set = {set}, task = {task}");
-//                 barrier.wait().await;
-//             });
-//         }
-//     }
+    for set in 0..sets {
+        // Setup a client to put into the load balancer
+        let client = OfficeConvertClient::new(client_url.as_str()).unwrap();
 
-//     barrier.wait().await;
-// }
+        // Setup the load balancer
+        let lb = OfficeConvertLoadBalancer::new([client]);
+        let lb = Arc::new(lb);
+
+        for task in 0..tasks {
+            let lb = lb.clone();
+            let barrier = barrier.clone();
+            let file = file.clone();
+            tokio::spawn(async move {
+                println!("start job set = {set}, task = {task}");
+                lb.convert(file).await.unwrap();
+                println!("end job set = {set}, task = {task}");
+                barrier.wait().await;
+            });
+        }
+    }
+
+    barrier.wait().await;
+}
